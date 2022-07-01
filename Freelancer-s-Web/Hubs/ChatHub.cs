7af16dcfde30cea7a43607888c562a1bba54aa77
phucvhd <75359238+phucvhd@users.noreply.github.com>
2022-07-01 11:22:21 +0700

    add chat hub

---
+++ Freelancer-s-Web/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Freelancer_s_Web.Hubs
+{
+	public class ChatHub
+	{
+		public ChatHub()
+		{
+		}
+	}
+}
+
---
+++ Freelancer-s-Web/Pages/Message/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Freelancer_s_Web.Models;
+
+namespace Freelancer_s_Web.Pages.Message
+{
+    public class IndexModel : PageModel
+    {
+        public IndexModel()
+        {
+            //_context = context;
+        }
+
+        public IList<Models.Message> Message { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            //Message = await _context.Messages
+            //    .Include(m => m.Receiver)
+            //    .Include(m => m.Sender).ToListAsync();
+        }
+    }
+}
